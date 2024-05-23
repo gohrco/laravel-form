7be0f3a14175ce55c959e09a0e978b477de01801
Steven Mueller <smueller@maxtongroup.com>
2024-05-23 19:31:51 -0400

    update content blade

--- src/resources/views/fields/content.blade.php
+++ src/resources/views/fields/content.blade.php
@@ -1,14 +1,7 @@
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 <!-- laravelform::fields.content START -->
-@php die('not done'); @endphp
-@if ( isset($data['nowrap'] ) && $data['nowrap'] )
-	{!! $data['content'] !!}
-@else
-	<div class="row"{{ ( isset ( $data['id'] ) ? ' id=' . $data['id']  : null ) }}>
-		<div class="{{ isset( $data['class'] ) ? $data['class'] : 'col-md-6 offset-md-6' }}">
-			{!! $data['content'] !!}
-		</div>
-	</div>
-@endif
+
+{!! $field->get('content') !!}
 
 <!-- laravelform::fields.content END -->
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
